ab976faf43347aac4bb6742990df81d3ceaf2e1d
pankajjyadav <139075959+pankajjyadav@users.noreply.github.com>
2024-07-08 21:31:48 +0000

    data Type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3)    // we are using nodeJS not browser Console
+
+
+let name = "pankaj"   // String data type
+let age = 18           // integer
+let isLoggedIn = false  // Boolean data type
+let state;
+
+// Primitive Data Type
+
+// number => 2 to power 53
+// bigInt
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object 
+
+// Kisi bhi chij ka type puchne ke liye use krte h 
+
+
+console.log(typeof age);   // integer ya number
+
+console.log(typeof null);       //  output milega => object
+console.log(typeof undefined);  //  output milega => undefine
