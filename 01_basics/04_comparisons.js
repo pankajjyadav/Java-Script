c3000b124b348d92bd68006721df0c9e377c847f
pankajjyadav <139075959+pankajjyadav@users.noreply.github.com>
2024-07-09 13:05:13 +0000

    Comparison Done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,30 @@
+// Boolean Comparison gives output in True/False
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2 != 1);
+
+
+
+console.log("2" > 1); 
+console.log("02" > 1);
+
+// in dono output True ayega  Ye comparison unpredictable result dete h 
+
+// so comparison ke time yaad rakhe same data Type m comparison kare
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// output =>  Flase , False , True   Ayega
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+
+// output iska False ayega teeno ka
