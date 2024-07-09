76f35cc4e43c70ef3a7b39ac623164634ce36edf
pankajjyadav <139075959+pankajjyadav@users.noreply.github.com>
2024-07-09 12:16:24 +0000

    Conversion of data Type

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,33 @@
+let score = "298abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);   // this is gives output # NAN - Not A Number coz "298abc" is not number
+
+
+
+// # Conversion 
+
+// "33" => 33
+// "33abc" => NaN - Not a Number
+
+// # boolean
+
+// true => 1; 
+// flase => 0;  
+// ""  => false  // Empty string 
+// "hitesh" => ture
+
+let isLoggedIn =1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);     // it gives " True"
+
+
+
+
