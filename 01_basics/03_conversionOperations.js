2a75e37e46792bc0b355a2113418fb002768180f
pankajjyadav <139075959+pankajjyadav@users.noreply.github.com>
2024-07-09 12:44:52 +0000

    conversion Operations done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+
+ console.log(negValue);
+
+
+  // basic operations
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "Pankaj"
+let str2 = " yadav"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);  // Phle String define kr diya to bad bale sabhi as String treat honge
+console.log(1 + 2 + "2");  // phle number add then String add hogi usme
+
+
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter;
+
+// upar dono m hi value bhadegi chahe Prefix ho ya Postfix but har jagah esa nhi hoga 
+
+console.log(gameCounter);
+
+
+
